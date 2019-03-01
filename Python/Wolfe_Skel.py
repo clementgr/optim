159ee421c3c710018d93022c31cc0168c4bff7ec
clems grisi <clementnet@outlook.com>
2019-03-01 10:54:42 +0100

    ajout gradient_V.py

--- Python/Wolfe_Skel.py
+++ Python/Wolfe_Skel.py
@@ -21,12 +21,19 @@ from numpy import dot
 #  Arguments en sortie
 #
 #    alphan : valeur du pas apres recherche lineaire
-#    ok     : indicateur de reussite de la recherche 
+#    ok     : indicateur de reussite de la recherche
 #             = 1 : conditions de Wolfe verifiees
 #             = 2 : indistinguabilite des iteres
 
+def first_wolfe_cond(alpha, xp, D, Oracle, w1):
+    argout_p = Oracle(xp, 4)
+    argout_n = Oracle(xp + alpha*D, 2)
+    return( (argout_n[0] - argout_p[0]) <= w1*alpha*np.dot(argout_p[1], D) )
+
+
+
 def Wolfe(alpha, x, D, Oracle):
-    
+
     ##### Coefficients de la recherche lineaire
 
     omega_1 = 0.1
@@ -39,29 +46,40 @@ def Wolfe(alpha, x, D, Oracle):
     dltx = 0.00000001
 
     ##### Algorithme de Fletcher-Lemarechal
-    
+
     # Appel de l'oracle au point initial
-    argout = Oracle(x)
+    argout = Oracle(x, 4)
     critere = argout[0]
     gradient = argout[1]
-    
+
     # Initialisation de l'algorithme
     alpha_n = alpha
     xn = x
-    
+
     # Boucle de calcul du pas
     while ok == 0:
-        
+
         # xn represente le point pour la valeur courante du pas,
         # xp represente le point pour la valeur precedente du pas.
         xp = xn
-        xn = x + alpha_n*D
-        
+        xn = xp + alpha_n*D
+
         # Calcul des conditions de Wolfe
         #
         # ---> A completer...
         # ---> A completer...
-        
+
+        # fisrt Wolfe rule
+        argout_p = Oracle(xp, 4)
+        critere_p = argout_p[0]
+        gradient_p = argout_p[1]
+        argout_n = Oracle(xn, 4)
+        critere_n = argout_p[0]
+        gradient_n = argout_p[1]
+
+        first_cond = (critere_n - critere_p) <= omega_1*alpha_n*np.dot(gradient_p, D)
+        second_cond = np.dot(gradient_n,d) >= omega_2*np.dot(gradient_p,d)
+
         # Test des conditions de Wolfe
         # - si les deux conditions de Wolfe sont verifiees,
         #   faire ok = 1 : on sort alors de la boucle while
@@ -69,7 +87,20 @@ def Wolfe(alpha, x, D, Oracle):
         #
         # ---> A completer...
         # ---> A completer...
-        
+
+        if (not first_cond):
+            alpha_max = alpha_n
+            alpha_n = (alpha_min + alpha_max)/2
+        else:
+            if (not second_cond):
+                alpha_min = alpha_n
+                if (alpha_max == np.inf):
+                    alpha_n = 2*alpha_min
+                else:
+                    alpha_n = (alpha_min + alpha_max)/2
+            else:
+                ok = 1
+
         # Test d'indistinguabilite
         if np.linalg.norm(xn - xp) < dltx:
             ok = 2
