d7269cc67227ad2f6019127a7274231c23dfdb70
Mathieu Lerouge <lerouge.mathieu91@gmail.com>
2019-03-01 11:22:58 +0100

    Format.

--- Python/OraclePG.py
+++ Python/OraclePG.py
@@ -16,29 +16,18 @@ from Verification import Verification
 
 def F(q):
     return np.dot(q,r*q*np.absolute(q))/3 + np.dot(pr,np.matmul(Ar,q))
-    #return np.vdot(q,r*q*np.abs(q))/3 + np.dot(pr,np.dot(Ar,q))
 
 def Q(qc):
     return q0+np.matmul(B,qc)
-    #return(q0 + np.dot(B,qc))
 
 def gradient_F_tilde(q):
     return r*q*np.absolute(q) + np.matmul(np.transpose(Ar),pr)
-    #return(r*q*np.abs(q) + np.dot(np.transpose(Ar),pr))
 
 def G(qc):
     return np.matmul(np.transpose(B),gradient_F_tilde(Q(qc)))
-    #return(np.dot(np.transpose(B), gradient_F_tilde(Q(qc))))
 
 def H(qc):
-    return 2*np.matmul(np.transpose(B), np.matmul(np.diag(r*np.abs(q)), B))
-#def H(qc):
-#    diag = np.zeros((n,n))
-#    q = Q(qc)
-#    for i in range(n):
-#        diag[i,i] = r[i]*q[i]
-#    H_int = 2*np.dot(diag, B)
-#    return(np.dot(np.transpose(B), H_int))
+    return 2*np.matmul(np.transpose(B), np.matmul(np.diag(r*np.absolute(q)), B))
 
 def OraclePG(qC,ind):
     if (ind == 2):
